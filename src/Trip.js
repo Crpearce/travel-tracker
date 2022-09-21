a3bf5484a6dca9f5419e3eded101ea70bf15e2fd
Colby Pearce <crpearce892@gmail.com>
2022-09-20 19:11:29 -0600

    update apiCalls

---
+++ src/Trip.js
@@ -0,0 +1,14 @@
+class Trip {
+    constructor(id, userID, destinationID, travelers, date, duration, status) {
+        this.id = id;
+        this.userID = userID;
+        this.destinationID = destinationID;
+        this.travelers = travelers;
+        this.date = date;
+        this.duration = duration;
+        this.status = status;
+        this.suggestActivities = [];
+    }
+}
+
+export default Trip;
--- src/scripts.js
+++ src/scripts.js
@@ -1,11 +1,32 @@
-// This is the JavaScript entry file - your code begins here
-// Do not delete or rename this file ********
-
-// An example of how you tell webpack to use a CSS (SCSS) file
-import './css/styles.css';
-
 // An example of how you tell webpack to use an image (also need to link to it in the index.html)
 import './images/turing-logo.png'
 
+import './css/styles.css';
+import { fetchData } from "./apiCalls";
+import Traveler from './Traveler';
+import Trip from './Trip';
+import Destination from './Destination';
+import Session from './Session';
 
-console.log('This is the JavaScript entry file - your code begins here.');
+// QUERYSELECTORS
+
+// GLOBAL VARIABLES
+
+// EVENT LISTENERS
+window.onload = (event) => {
+    getData();
+    // showLoginView();
+  };
+
+// FUNCTION
+
+const getData = () => {
+    Promise.all([fetchData("travelers"),fetchData("trips"),fetchData("destinations"),])
+    .then((value) => {
+        travelersData = value[0].travelers;
+        tripsData = value[1].trips;
+        destinationsData = value[2].destinations;
+        session = new Session(travelersData, tripsData, destinationsData);
+        console.log(session)
+    });
+  }
